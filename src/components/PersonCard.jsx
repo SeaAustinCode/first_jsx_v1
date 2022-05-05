80e4be0d61122f482421b31dbb1e5c929f79fdaa
SeaAustinCode <seaaustincode@gmail.com>
2022-05-04 19:32:34 -0700

    updated-birthday-button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,12 +1,27 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component {
-    render() { //need a constructor that takes in the props 
+
+    //Declare constructor to override our default constructor so that we can initially set the state object 
+    constructor(props) {
+        //super gives us all the functionality of the default constructor that comes with Component
+        super(props);
+        //now we can set our state object 
+        this.state = {
+            age: this.props.person.age
+        }
+    }
+    birthday = () => {
+        this.setState({ age: this.state.age + 1 })
+    }
+
+    render() {
         return (
             <div>
                 <h1>{this.props.person.lastName}, {this.props.person.firstName}</h1>
-                <p>Age: {this.props.person.age}</p>
+                <p>Age: {this.state.age}</p> 
                 <p>Hair Color: {this.props.person.hairColor}</p>
+                <button onClick={ () => this.birthday() }>Happy Birthday!</button>
             </div>
         );
     }
